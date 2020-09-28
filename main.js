2d6251d820656ea6db78412e7ef79020a5dc0651
Safeer Khan <thesafeerkhan@gmail.com>
2020-09-28 01:09:38 -0400

    Implemented the API Functionality

--- main.js
+++ main.js
@@ -18,10 +18,36 @@ searchBox.addEventListener("keypress", function (event) {
 });
 
 searchButton.addEventListener("click", function () {
-	getWeather(searchButton.value);
+	getWeather(searchBox.value);
 });
 
 // Functions
-function getWeather(locationName) {
-	alert("Hello");
+function getWeather(locationQuery) {
+	fetch("https://api.openweathermap.org/data/2.5/weather?q=" + locationQuery + "&units=metric&appid=" + apiKey)
+		.then(function (weather) {
+			return weather.json();
+		})
+		.then(updateWeather);
+}
+
+function updateWeather(weatherObject) {
+	locationName.innerText = weatherObject.name + ", " + weatherObject.sys.country;
+	locationWeather.innerText = weatherObject.weather[0].description;
+	locationTemp.innerText = Math.round(weatherObject.main.temp) + "°c";
+	locationHighLow.innerText = Math.round(weatherObject.main.temp_min) + "°c / " + Math.round(weatherObject.main.temp_max) + "°c";
+	//Date
+	let now = new Date();
+	locationDate.innerHTML = dateBuilder(now);
+}
+
+function dateBuilder(d) {
+	let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	let days = ["Sunday", "Monday", "Tuesday", "Wedday", "Thursday", "Friday", "Saturday"];
+
+	let day = days[d.getDay()];
+	let date = d.getDate();
+	let month = months[d.getMonth()];
+	let year = d.getFullYear();
+
+	return `${day}, ${date} ${month}, ${year}`;
 }
